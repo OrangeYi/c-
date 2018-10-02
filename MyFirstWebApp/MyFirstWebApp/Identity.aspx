8f202960395457b3d8bd353a22621961663f830a
Ziyi Chen <q363389749@gmail.com>
2018-10-01 21:49:15 -0400

    web gamble game

---
+++ MyFirstWebApp/MyFirstWebApp/Identity.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Identity.aspx.cs" Inherits="MyFirstWebApp.Identity" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Rule: Guess a number betweent 1-99 and the computer will generate a random number.<br />
+            If your gusee number less then the random number you win.<br />
+            For example your guess number is 50 and the random number is 60 then you win.<br />
+            However if the random number is 49 you lose.<br />
+            <br />
+            Guess a number:
+            <asp:TextBox ID="guessNumber" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+            <br />
+            <br />
+            Make a bet:
+            <asp:TextBox ID="betBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="okButton" runat="server" OnClick="okButton_Click" Text="Click And Win" />
+            <br />
+            <br />
+            Add fund: <asp:TextBox ID="fundBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addFund" runat="server" OnClick="addFund_Click" Text="Add fund" />
+            <br />
+            <br />
+            Amount:
+            <asp:Label ID="amountLabel" runat="server" Text="0"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="resultLabel" runat="server"></asp:Label>
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="debug" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyFirstWebApp/MyFirstWebApp/Identity.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyFirstWebApp
+{
+    public partial class Identity : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void okButton_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(guessNumber.Text) || string.IsNullOrEmpty(betBox.Text))
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Please fill a number between 1-99 or make a bet!!');</script>");
+                return;
+            }
+            if(Convert.ToInt32(betBox.Text) > Convert.ToInt32(amountLabel.Text))
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Your bet is over your amount!!!');</script>");
+                return;
+            }
+            if(amountLabel.Text.CompareTo("0") == 0)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script>alert('Not enough money please add fund!!!');</script>");
+                return;
+            }
+            int guessnumber = Convert.ToInt32(guessNumber.Text);
+
+            Random ran = new Random();
+            int randomnumber = ran.Next(1, 99);
+            if(guessnumber > randomnumber)
+            {
+                float win = ((100f - guessnumber) / 100f) * (1 + Convert.ToInt32(betBox.Text)) + Convert.ToInt32(amountLabel.Text);
+                int winint = (int)win;
+                amountLabel.Text = winint.ToString();
+                resultLabel.Text = "Your win and the random is " + randomnumber;
+            }
+            else
+            {
+                resultLabel.Text = "Your lose and the random is " + randomnumber;
+                int resultamount = Convert.ToInt32(amountLabel.Text) - Convert.ToInt32(betBox.Text);
+                amountLabel.Text = resultamount.ToString();
+            }
+        }
+
+        protected void addFund_Click(object sender, EventArgs e)
+        {
+            int addedfund = Convert.ToInt32(fundBox.Text);
+
+            int resultfund = addedfund + Convert.ToInt32(amountLabel.Text);
+
+            amountLabel.Text = resultfund.ToString();
+        }
+    }
+}
